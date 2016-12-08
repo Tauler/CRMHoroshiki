c8cc644182a5d7d3ad9819de37258ee1ca88e84d
onyushkindv <dmitry.onyushkin@yandex.ru>
2016-12-08 12:34:22 +0400

    Сервисы добавленея, удаления, редактирования телефона пользователя

--- backend/site/src/main/java/ru/horoshiki/crm/site/controller/UserController.java
+++ backend/site/src/main/java/ru/horoshiki/crm/site/controller/UserController.java
@@ -4,15 +4,14 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.HtmlUtils;
 import ru.horoshiki.crm.sendsms.SmsSender;
 import ru.horoshiki.crm.site.model.dto.BackendData;
 import ru.horoshiki.crm.site.model.dto.UserSmallDTO;
 import ru.horoshiki.crm.site.model.entity.Phone;
 import ru.horoshiki.crm.site.model.entity.User;
+import ru.horoshiki.crm.site.service.PhoneService;
 import ru.horoshiki.crm.site.service.UserService;
 import ru.horoshiki.crm.site.utils.SmsUtils;
 
@@ -34,6 +33,8 @@ public class UserController {
     private SmsSender smsSender;
     @Autowired
     private UserService userService;
+    @Autowired
+    private PhoneService phoneService;
 
     @RequestMapping(value = "/getCurrentUser", method = RequestMethod.GET)
     public
@@ -56,7 +57,7 @@ public class UserController {
     public
     @ResponseBody
     BackendData addPhone(@RequestParam(value = "phone", required = true) String phoneStr){
-        User user = userService.getUserByLogin(SecurityContextHolder.getContext().getAuthentication().getName(), "phones");
+        User user = userService.getUserByLogin(SecurityContextHolder.getContext().getAuthentication().getName());
         if(user==null)
             return BackendData.error("userNotFoundError");
 
@@ -64,15 +65,68 @@ public class UserController {
         Phone phone = new Phone();
         phone.setNewPhone(phoneStr);
         phone.setConfirmCode(code);
-        smsSender.send("7".concat(user.getLogin()), "Код подтверждения: "+code);
-        if(user.getPhones()==null)
-            user.setPhones(new ArrayList<>());
+        smsSender.send("7".concat(phone.getNewPhone()), "Код подтверждения: "+code);
         phone.setUser(user);
-        user.getPhones().add(phone);
+        phoneService.update(phone);
+        return BackendData.success(phone.getId());
+    }
 
-        userService.update(user);
+    @RequestMapping(value = "/editPhone", method = RequestMethod.GET)
+    public
+    @ResponseBody
+    BackendData editPhone(@RequestParam(value = "phoneId", required = true) long phoneId,
+                          @RequestParam(value = "phone", required = true) String newPhone){
+
+
+        User user = userService.getUserByLogin(SecurityContextHolder.getContext().getAuthentication().getName(), "phones");
+        if(user==null)
+            return BackendData.error("userNotFoundError");
+        Phone phone = phoneService.get(phoneId);
+        if(phone==null)
+            return BackendData.error("phoneNotFoundError");
+        if(!user.getPhones().contains(phone))
+            return BackendData.error("userNotAccessError");
+        String code = SmsUtils.generateCode();
+        phone.setNewPhone(HtmlUtils.htmlEscape(newPhone));
+        phone.setConfirmCode(code);
+        smsSender.send("7".concat(phone.getNewPhone()), "Код подтверждения: "+code);
+        phone.setUser(user);
+        phoneService.update(phone);
 
         return BackendData.success(phone.getId());
     }
 
+    @RequestMapping(value = "/confirmPhone", method = RequestMethod.POST)
+    public
+    @ResponseBody
+    BackendData confirmPhone(@RequestParam(value = "phoneId", required = true) long phoneId,
+                             @RequestParam(value = "code", required = true) String code){
+        Phone phone = phoneService.get(phoneId);
+        if(phone==null)
+            return BackendData.error("phoneNotFoundError");
+        if(code == null || "".equals(code) || !code.equals(phone.getConfirmCode()))
+            return BackendData.error("invalidCodeError");
+        phone.setPhone(phone.getNewPhone());
+        phone.setConfirmCode(null);
+        phone.setNewPhone(null);
+        phoneService.update(phone);
+
+        return BackendData.success(true);
+    }
+
+    @RequestMapping(value = "/delPhone", method = RequestMethod.POST)
+    public
+    @ResponseBody
+    BackendData delPhone(@RequestParam(value = "phoneId", required = true) long phoneId){
+        User user = userService.getUserByLogin(SecurityContextHolder.getContext().getAuthentication().getName(), "phones");
+        if(user==null)
+            return BackendData.error("userNotFoundError");
+        Phone phone = phoneService.get(phoneId);
+        if(phone==null)
+            return BackendData.error("phoneNotFoundError");
+        if(!user.getPhones().contains(phone))
+            return BackendData.error("userNotAccessError");
+        phoneService.delete(phone);
+        return BackendData.success(true);
+    }
 }
